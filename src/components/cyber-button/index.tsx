247eb79f45f56534475cff2091490cab0a0ef361
alpacachen <alpacachen@users.noreply.github.com>
2021-12-30 08:58:25 +0800

    button width height

--- src/components/cyber-button/index.tsx
+++ src/components/cyber-button/index.tsx
@@ -1,16 +1,16 @@
-import {FC} from "react";
-import './index.css'
+import React, {FC} from "react";
+import './index.scss'
 import '../common.css'
 import 'augmented-ui/augmented-ui.min.css'
 
 export const CyberButton:FC<{
-  // /**
-  //  * asfassa
-  //  */
-  // test?: boolean
+  width?: string
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
 }> = (props) => {
-  return <button className='cyber-button' data-augmented-ui="bl-clip" data-text={props.children} >
-    {props.children}
-    <div className='cyber-button-after animate'>{props.children}</div>
+  console.log(props, 44)
+  return <button onClick={props.onClick} className='cyber-button-container' data-augmented-ui="bl-clip" data-text={props.children} >
+    <div className='cyber-button' style={{height: '100px', width: props.width}}>{props.children}</div>
+    <div className='cyber-button-filter'/>
+    <div className='cyber-button-after'>{props.children}</div>
   </button>
 }
--- src/stories/CyberButton.stories.tsx
+++ src/stories/CyberButton.stories.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Story, Meta } from '@storybook/react';
 import {CyberButton} from "../components/cyber-button";
+import '../App.css'
 
 export default {
     title: 'Example/cyber-button',
     component: CyberButton,
     argTypes: {
-        children: {
-            control: 'text'
-        }
+        // children: {control: 'text'},
+        width: {control: 'text'},
     }
 } as Meta
 
-export const Template: Story<Parameters<typeof CyberButton>[0]> = (args) => <CyberButton>{args.children}</CyberButton>
-Template.args = {children: 'submit'}
+export const Template: Story<Parameters<typeof CyberButton>[0]> = ({children, ...rest}) => <CyberButton {...rest}>{children}</CyberButton>
+Template.args = {
+    children: 'submit333',
+    width: '20px'
+}
