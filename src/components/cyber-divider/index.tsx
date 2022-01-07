0a755c5b6443c1e7f86e69391494708910cfe010
alpacachen <alpacachen@users.noreply.github.com>
2022-01-07 18:02:10 +0800

    ADD: cyber-divider

--- src/components/cyber-divider/index.tsx
+++ src/components/cyber-divider/index.tsx
@@ -1,31 +1,26 @@
 import {FC, useMemo} from "react";
 import './index.scss'
-import TopBlack from '../assets/top-black.svg'
-import BottomBlack from '../assets/bottom-black.svg'
-import BottomYellow from '../assets/bottom-yellow.svg'
-import TopYellow from '../assets/top-yellow.svg'
 
 export const CyberDivider:FC<{
     color: 'yellow' | 'black',
     direction: 'top' | 'bottom'
 }> = (props) => {
     const { color, direction } = props
-    const src = useMemo(() => {
+    const str = useMemo(() => {
         if (color === 'yellow'){
             if(direction === 'top'){
-                return TopYellow
+                return 't-y'
             }else {
-                return BottomYellow
+                return 'b-y'
             }
         }else {
             if(direction === 'top'){
-                return TopBlack
+                return 't-b'
             }else {
-                return BottomBlack
+                return 'b-b'
             }
         }
     }, [direction, color])
-    return <div className='cyber-divider' style={{fontSize: 0}}>
-        <img style={{width: '100%'}} src={src} alt=''/>
+    return <div className={`cyber-divider ${str}`}>
     </div>
 }
